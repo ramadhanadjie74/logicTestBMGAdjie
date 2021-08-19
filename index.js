8eb144b90a77adfec7d9ccaf7fadd5c76034a410
ramadhanadjie74 <adjiefr@gmail.com>
2021-08-19 10:09:13 +0700

    logic test on 19/08/21 Adjie Fajar Ramadhan
    .

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let i = 0;
+// var number  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+//                 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+
+// let ubah = number/3
+
+
+hitung = function(){
+    if (i = 0, i < 50, i++){
+        console.log(i);
+    }
+}
+
+console.log(hitung);
+
+// hitung = function(e) {
+//     if (number, number === 3, (e)){
+//         e = 
+//     }
+// }
+
+
+        // if (i, i < 50, i++) {
+        //     if (i, i === 3 && i === 5, (e))
+        //     {
+                
+        //     }
+        // }
+
+
+// jika number bisa dibagi 3 => ubah jadi Frontend
+// jika number bisa dibagi 5 => ubah jadi Backend
+// jika number bisa dibagi 3 dan 5 => ubah jadi Frontend Backend
+// const num = '2'
+// console.log('satu');
